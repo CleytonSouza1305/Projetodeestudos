534e14c4007c905e54a670beb63c9a087e1f0b4b
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2023-10-06 18:55:30 -0300

    fiz o inicio da main

---
+++ Phone Store/src/js/script.js
@@ -0,0 +1,32 @@
+var ios = document.querySelectorAll('.ios-btn')
+
+var android = document.querySelectorAll('.android-btn')
+
+function alterarClasseClicando () {
+    var temClasseClicado = 
+    this.classList.contains("clicado")
+
+    ios.forEach(function(btns) {
+        btns.classList.remove("clicado")
+    })
+
+    android.forEach(function(btns) {
+        btns.classList.remove("clicado")
+    })
+
+    if (!temClasseClicado) {
+        this.classList.add("clicado")
+    }
+}
+
+ios.forEach(function(btns) {
+    btns.addEventListener("click", alterarClasseClicando)
+})
+
+android.forEach(function(btns) {
+    btns.addEventListener("click", alterarClasseClicando)
+})
+
+
+
+
