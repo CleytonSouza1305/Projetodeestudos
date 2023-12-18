fc5517863e0ebdbab4106f06d91ff6f6f037af37
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2023-12-18 18:58:52 -0300

    atualizações gerais...

---
+++ Cronômetro/script.js
@@ -0,0 +1,10 @@
+const hour = document.querySelector('.hour')
+const minuts = document.querySelector('.minuts')
+const seconds = document.querySelector('.seconds')
+const milis = document.querySelector('.miliseconds')
+
+const startbtn = document.getElementById('iniciar')
+
+
+
+
